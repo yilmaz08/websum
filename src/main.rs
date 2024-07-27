62f7b199097b9227e6e7e6d91c2a415cc64cbece
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-07-27 16:31:35 +0300

    chore: init
    main.rs
    
    sha256sums for arch linux 2024.05.01, 2024.06.01 and 2024.07.01

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::process::Command;
+use std::str;
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = args().nth(1).expect("No file path provided");
+
+    let output = Command::new("sha256sum")
+        .arg(path)
+        .output()
+        .expect("Failed to execute sha256sum");
+
+    if !output.status.success() {
+        let stderr = str::from_utf8(&output.stderr).expect("Failed to read stderr");
+        eprintln!("Error: {}", stderr);
+        return Ok(());
+    }
+
+    let stdout = str::from_utf8(&output.stdout).expect("Failed to read stdout");
+    let sha256 = stdout.split_whitespace().next().expect("Failed to get sha256sum").to_string().to_uppercase();
+    println!("SHA256SUM: {}", sha256);
+    println!("Processing...");
+
+    let url = format!("https://raw.githubusercontent.com/yilmaz08/websum/main/archive/{}", sha256);
+    
+    let response = reqwest::blocking::get(url)?;
+
+    if response.status() == 404 {
+        eprintln!("This file is not found in our archive! It is either invalid or not in our archive.");
+        return Ok(());
+    }
+
+    // println!("Response: {:#?}", response);
+    println!("This file is a valid {} file.", response.text()?);
+
+    Ok(())
+}
