4653dcd979f9981649f4bd3d7a8a4405fac0b199
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-07-30 03:12:49 +0300

    fix: better error handling
    Adds "print_error" function
    
    Removes file existance check, instead the error is handled on File::open

--- src/main.rs
+++ src/main.rs
@@ -23,40 +23,42 @@ fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest, Box<dyn Error>> {
 
     loop {
         let count = reader.read(&mut buffer)?;
-        if count == 0 {
-            break;
-        }
+        if count == 0 { break; }
         context.update(&buffer[..count]);
     }
 
     return Ok(context.finish());
 }
 
+fn print_error(err: std::io::Error) {
+    match err.kind() {
+        std::io::ErrorKind::NotFound => eprintln!("Error: File not found!"),
+        std::io::ErrorKind::PermissionDenied => eprintln!("Error: Permission denied!"),
+        _ => eprintln!("Unknown Error: {}", err)
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Parse Arguments
     let args = Args::parse();
-    
-    // Check if the file exists
-    if !std::path::Path::new(&args.path).exists() {
-        eprintln!("Error: The file was not found!");
-        std::process::exit(1);
-    }
 
     // Print "Processing..."
-    if !args.short {
-        println!("Processing...");
-    }
+    if !args.short { println!("Processing..."); }
 
     // SHA256
-    let file = File::open(&args.path)?;
+    let file = match File::open(&args.path) {
+        Ok(file) => file,
+        Err(err) => {
+            if !args.short { print_error(err); }
+            std::process::exit(1);
+        }
+    };
     let reader = BufReader::new(file);
     let digest = sha256_digest(reader)?;
     let sha256 = HEXUPPER.encode(digest.as_ref());
     
     // Print SHA256 Result
-    if !args.short {
-        println!("SHA256: {}", sha256);
-    }
+    if !args.short { println!("SHA256: {}", sha256); }
     
     // Request
     let url = format!("https://raw.githubusercontent.com/yilmaz08/websum/main/archive/{}", sha256);
@@ -64,9 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // UNSUCCESSFUL
     if response.status() == 404 {
-        if !args.short {
-            eprintln!("This file is not found in our archive! It is either invalid or not in our archive.");
-        }
+        if !args.short { eprintln!("This file is not found in our archive! It is either invalid or not in our archive."); }
         std::process::exit(1);
     }
 
